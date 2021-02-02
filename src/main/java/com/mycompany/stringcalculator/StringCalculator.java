bed0094c42b46035f0940b3caee5387336e15657
Sachin Sharma <sachin.sharma100197@gmail.com>
2021-02-02 23:48:27 +0530

    Add code for negative numbers and numbers getter than 1000 and write test case for them

--- src/main/java/com/mycompany/stringcalculator/StringCalculator.java
+++ src/main/java/com/mycompany/stringcalculator/StringCalculator.java
@@ -2,18 +2,35 @@ package com.mycompany.stringcalculator;
 
 public class StringCalculator {
 
+  private static int count = 0;
+
   public int add(String numbers) {
+    GetCalledCount();
     int sum = 0;
 
-    String delimiter = Character.toString(numbers.charAt(2));
-    String numberStr = numbers.substring(numbers.indexOf("\n") + 1, numbers.length());
+//    String delimiter = Character.toString(numbers.charAt(2));
+//    String numberStr = numbers.substring(numbers.indexOf(",\n") + 1, numbers.length());
 
-    if (!numberStr.isEmpty()) {
-      String[] arr = numberStr.replace("\n", delimiter).trim().split(delimiter);
+    if (!numbers.isEmpty()) {
+      String[] arr = numbers.split(",|\n");
       for (String number : arr) {
-        sum += Integer.parseInt(number);
+        sum += checkNumber(number);
       }
     }
     return sum;
+  }
+
+  public int checkNumber(String number) {
+    int num = Integer.parseInt(number);
+    if (num < 0) {
+      throw new IllegalArgumentException("Negative Number: " + number);
+    } else if (num > 1000) {
+      num = 0;
+    }
+    return num;
+  }
+
+  public void GetCalledCount() {
+    count++;
   }
 }
--- src/test/java/com/mycompany/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/mycompany/stringcalculator/StringCalculatorTest.java
@@ -23,13 +23,65 @@ public class StringCalculatorTest {
   }
 
   @Test
-  @DisplayName("Add Numbers")
-  public void addNumbersWithEmptyString() {
+  @DisplayName("Add Numbers with empty string")
+  public void addNumbersWithEmptyString_shouldReturn0() {
+    testReporter.publishEntry("Running " + testInfo.getDisplayName());
+    String numbers = "";
+    StringCalculator calculator = new StringCalculator();
+    int sum = calculator.add(numbers);
+    assertEquals(0, sum, "Test runs successfully by passing empty string");
+  }
+  
+  @Test
+  @DisplayName("Add Numbers with multiple numbers")
+  public void addNumbersWithMultipleNumberShouldReturnSum() {
+    testReporter.publishEntry("Running " + testInfo.getDisplayName());
+    String numbers = "1,2,3";
+    StringCalculator calculator = new StringCalculator();
+    int sum = calculator.add(numbers);
+    assertEquals(6, sum, "Test runs successfully by adding multiple numbers");
+  }
+
+  @Test
+  @DisplayName("Add Numbers with new line")
+  public void addNumbersWithNumberNewLineShouldReturnSum() {
+    testReporter.publishEntry("Running " + testInfo.getDisplayName());
+    String numbers = "1\n2,3";
+    StringCalculator calculator = new StringCalculator();
+    int sum = calculator.add(numbers);
+    assertEquals(6, sum, "Test runs successfully by passing new line with numbers");
+  }
+
+  @Test
+  @DisplayName("Add Numbers with different delimiters")
+  public void addNumbersWithNumberWithDifferentDelimiterShouldReturnSum() {
     testReporter.publishEntry("Running " + testInfo.getDisplayName());
     String numbers = "//;\n1;2\n3";
     StringCalculator calculator = new StringCalculator();
     int sum = calculator.add(numbers);
-    assertEquals(6, sum, "Test runs successfully by adding multiple numbers");
+    assertEquals(6, sum, "Test runs successfully by passing different delimiter and multiple numbers");
+  }
+  
+  @Test
+  @DisplayName("Add Negative Numbers")
+  public void addNumbersWithNegativeNumber_shouldThrowException() {
+    testReporter.publishEntry("Running " + testInfo.getDisplayName());
+    String numbers = "-1,-2";
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> new StringCalculator().add(numbers));
+
+    String expectedMessage = "Negative Number: ";
+    String actualMessage = exception.getMessage();
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+  
+  @Test
+  @DisplayName("Add Numbers with number greater than 1000")
+  public void addNumbersWithNumberWithNumber1000ShouldReturnSum() {
+    testReporter.publishEntry("Running " + testInfo.getDisplayName());
+    String numbers = "1,1001";
+    StringCalculator calculator = new StringCalculator();
+    int sum = calculator.add(numbers);
+    assertEquals(1, sum, "Test runs successfully by passing different number greater than 1000");
   }
 
 }
